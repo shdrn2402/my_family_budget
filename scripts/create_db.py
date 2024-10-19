002403fd51206b793825d529894858eb856cd0cc
Andrey Shadrin <shdrn@yandex.ru>
2024-10-19 17:37:20 +0300

    Add some librories to equirements.txt

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,7 +1,8 @@
-import psycopg2
+import psycopg
 import logging
 from dotenv import load_dotenv
 import os
+import re
 
 # Настройка логирования
 logging.basicConfig(filename='logs/app.log',
@@ -14,7 +15,8 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 # Получение значений переменных окружения
-DBNAME = os.getenv('DBNAME')
+ROOT_DBNAME = os.getenv('ROOT_DBNAME')
+DBNAMETOCREATE = os.getenv('NEW_DBNAME')
 ROOT_USER = os.getenv('ROOT_USER')
 ROOT_PASSWORD = os.getenv('ROOT_PASSWORD')
 MAIN_USER = os.getenv('MAIN_USER')
@@ -27,108 +29,126 @@ HOST = os.getenv('HOST')
 PORT = os.getenv('PORT')
 
 
-def create_database(db_name):
+def create_database(root_db_name, root_user, root_password, host, port,
+                    db_name_to_create):
     """
-    Создает базу данных, если она еще не существует.
+    Creates a new database if it does not already exist.
 
     Args:
-        db_name: Имя создаваемой базы данных.
+        root_db_name: The name of the default system database used for administrative tasks.
+        root_user: The superuser's username to connect to the database.
+        root_password: The superuser's password for the connection.
+        host: The database server's host.
+        port: The port number where the PostgreSQL server is running.
+        db_name_to_create: The name of the database to be created.
     """
 
-    # Подключение к базе данных postgres для создания новой базы данных
-    conn = psycopg2.connect(
-        database="postgres",
-        user=USER,
-        password=PASSWORD,
-        host=HOST,
-        port=PORT
-    )
+    logger.info(f"Creating database {db_name_to_create}...")
 
-    # Отключение транзакционного блока
-    conn.autocommit = True
-    cursor = conn.cursor()
+    # Checks the database name
+    if re.match(r'^[a-zA-Z0-9_]+$', db_name_to_create):
+        # Connect to an existing database
+        with psycopg.connect(
+            dbname=root_db_name,
+            user=root_user,
+            password=root_password,
+            host=host,
+            port=port
+        ) as conn:
+            # Set autocommit
+            conn.autocommit = True
+            # Open a cursor to perform database operations
+            with conn.cursor() as cursor:
+                # Execute a command: this creates a new database
+                try:
+                    cursor.execute(f"CREATE DATABASE {db_name_to_create}")
+                    logger.info(f"Database {db_name_to_create} created successfully.")
+                except psycopg.errors.DuplicateDatabase:
+                    logger.info(f"Database {db_name_to_create} already exists.")
+                except Exception as e:
+                    logger.error(f"Error while creating the database {db_name_to_create}: {str(e)}")
+                finally:
+                    # Commit the changes to the database
+                    conn.commit()
 
-    try:
-        cursor.execute(f"CREATE DATABASE {db_name}")
-        logger.info(f"База данных {db_name} создана успешно.")
-    except psycopg2.errors.DuplicateDatabase:
-        logger.info(f"База данных {db_name} уже существует.")
-    except Exception as e:
-        logger.error(f"Ошибка при создании базы данных {db_name}: {str(e)}")
-    finally:
-        cursor.close()
-        conn.close()
+    else:
+        logger.error("Invalid database name")
+        raise ValueError("Invalid database name")
 
 
-def create_table(conn, table_name, sql_query):
-    """
-    Проверяет наличие таблицы и создает ее, если она не существует.
+# def create_table(conn, table_name, sql_query):
+#     """
+#     Проверяет наличие таблицы и создает ее, если она не существует.
 
-    Args:
-        conn: Соединение с базой данных.
-        table_name: Имя создаваемой таблицы.
-        sql_query: SQL-запрос для создания таблицы.
-    """
+#     Args:
+#         conn: Соединение с базой данных.
+#         table_name: Имя создаваемой таблицы.
+#         sql_query: SQL-запрос для создания таблицы.
+#     """
 
-    cursor = conn.cursor()
+#     cursor = conn.cursor()
 
-    try:
-        # Проверка существования таблицы
-        cursor.execute(f"""SELECT EXISTS (SELECT FROM information_schema.tables
-                       WHERE table_name = '{table_name}');""")
-        exists = cursor.fetchone()[0]
+#     try:
+#         # Проверка существования таблицы
+#         cursor.execute(f"""SELECT EXISTS (SELECT FROM information_schema.tables
+#                        WHERE table_name = '{table_name}');""")
+#         exists = cursor.fetchone()[0]
 
-        if exists:
-            logger.info(f"Таблица {table_name} уже существует.")
-        else:
-            # Установка схемы по умолчанию
-            cursor.execute("SET search_path TO budget;")
+#         if exists:
+#             logger.info(f"Таблица {table_name} уже существует.")
+#         else:
+#             # Установка схемы по умолчанию
+#             cursor.execute("SET search_path TO budget;")
 
-            # Создание таблицы
-            cursor.execute(sql_query)
-            conn.commit()  # Сохранение изменений в базе данных
-            logger.info(f"Таблица {table_name} создана успешно.")
-    except Exception as e:
-        logger.error(f"Ошибка при создании таблицы {table_name}: {str(e)}")
-    finally:
-        cursor.close()
-
+#             # Создание таблицы
+#             cursor.execute(sql_query)
+#             conn.commit()  # Сохранение изменений в базе данных
+#             logger.info(f"Таблица {table_name} создана успешно.")
+#     except Exception as e:
+#         logger.error(f"Ошибка при создании таблицы {table_name}: {str(e)}")
+#     finally:
+#         cursor.close()
 
 def main():
     # Создаем базу данных, если она не существует
-    create_database(DBNAME)
+    create_database(root_db_name=ROOT_DBNAME,
+                    root_user=ROOT_USER,
+                    root_password=ROOT_PASSWORD,
+                    host=HOST,
+                    port=PORT,
+                    db_name_to_create=DBNAMETOCREATE)
 
     # Подключаемся к базе данных
-    conn = psycopg2.connect(
-        database=DBNAME,
-        user=USER,
-        password=PASSWORD,
-        host=HOST,
-        port=PORT
-    )
+    # conn = psycopg2.connect(
+    #     database=DBNAME,
+    #     user=USER,
+    #     password=PASSWORD,
+    #     host=HOST,
+    #     port=PORT
+    # )
 
-    # SQL-запросы для создания таблиц
-    create_users_table = """
-    CREATE TABLE IF NOT EXISTS users (
-        tg_id INTEGER PRIMARY KEY,
-        user_name VARCHAR(50) NOT NULL,
-        family_id INTEGER NOT NULL,
-        language VARCHAR(10) NOT NULL
-    );
-    """
+    # # SQL-запросы для создания таблиц
+    # create_users_table = """
+    # CREATE TABLE IF NOT EXISTS users (
+    #     tg_id INTEGER PRIMARY KEY,
+    #     user_name VARCHAR(50) NOT NULL,
+    #     family_id INTEGER NOT NULL,
+    #     language VARCHAR(10) NOT NULL
+    # );
+    # """
 
-    create_roles_table = """
-    CREATE TABLE IF NOT EXISTS roles (
-        id SERIAL PRIMARY KEY,
-        role_name VARCHAR(50) NOT NULL
-    );
-    """
+    # create_roles_table = """
+    # CREATE TABLE IF NOT EXISTS roles (
+    #     id SERIAL PRIMARY KEY,
+    #     role_name VARCHAR(50) NOT NULL
+    # );
+    # """
 
-    # Создаем таблицы
-    create_table(conn, "users", create_users_table)
-    create_table(conn, "roles", create_roles_table)
+    # # Создаем таблицы
+    # create_table(conn, "users", create_users_table)
+    # create_table(conn, "roles", create_roles_table)
 
-    conn.close()
+    # conn.close()
 
 
 if __name__ == "__main__":
